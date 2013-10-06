7b887999713ac44ea400c82467566619fc30a2ed
Radu Busuioc <ra.busuioc@gmail.com>
2013-10-06 11:41:29 +0100

    Strategy

---
+++ strategy/formatters.rb
@@ -0,0 +1,21 @@
+module Formatters
+  HTML_FORMATTER = lambda do |context|
+    puts '<html>'
+    puts '<head>'
+    puts "<title>#{@title}</title>"
+    puts '</head>'
+    puts '<body>'
+    context.text.each do |line|
+      puts "<p>#{line}</p>"
+    end
+    puts '</body>'
+    puts '</html>'
+  end
+
+  PLAIN_TEXT_FORMATTER = lambda do |context|
+    puts "*** #{@title} ***"
+    context.text.each do |line|
+      puts line
+    end
+  end
+end
